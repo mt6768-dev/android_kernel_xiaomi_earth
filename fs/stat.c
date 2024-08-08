3a30e28651f7512062a54c1038f049f1fcd2af9d
sidex15 <moses.limbo@gmail.com>
2024-08-08 09:04:21 +0800

    Add KernelSU Support
    Change-Id: I4085eb8ad4268ea10d506f42f25bb850c1fade6d

--- fs/stat.c
+++ fs/stat.c
@@ -148,6 +148,10 @@ int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 }
 EXPORT_SYMBOL(vfs_statx_fd);
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
+
 /**
  * vfs_statx - Get basic and extra attributes by filename
  * @dfd: A file descriptor representing the base dir for a relative filename
